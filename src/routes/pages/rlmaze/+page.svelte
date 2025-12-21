4600903df6cb34b9e09e5b13d83e9da0b09cc49d
Sophia Liu <75860082+soaphia14@users.noreply.github.com>
2025-12-21 01:36:54 -0500

    update

--- src/routes/pages/rlmaze/+page.svelte
+++ src/routes/pages/rlmaze/+page.svelte
@@ -1,12 +1,8 @@
 <script>
-    import { divclass } from "../../const";
-    import Tag from "../../Tag.svelte";
-
     var imgclass = "border-2 border-white rounded-lg md:w-4/5 mx-auto";
     var grayedclass = "text-white/50"
 </script>
 <h1>RL Maze</h1>
-<span class="flex"><Tag>Summer 2025</Tag></span>
 
 <br>
 <h2>Overview</h2>
